1ec362b6db2acf09b47d36a08acb89435e224983
cipher-tech <nickchibuikem@gmail.com>
2023-09-20 23:08:33 +0100

    added fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import { FontInter } from '@/assets/fonts';
+
+import { cn } from '@/utils';
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -7,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <main className={cn(FontInter.variable, 'font-inter', "min-h-screen")}>
+        <Component {...pageProps} />
+      </main>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
 
