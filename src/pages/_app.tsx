b89ab93cb9e3b17f1e95d2863ad90847602873c2
cipher-tech <nickchibuikem@gmail.com>
2023-09-23 12:53:01 +0100

    setup toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
-import { FontInter } from '@/assets/fonts';
-
+import "react-toastify/dist/ReactToastify.css";
 import { cn } from '@/utils';
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from "react-query";
+import { FontInter } from '@/assets/fonts';
 import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -13,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className={cn(FontInter.variable, 'font-inter', "min-h-screen")}>
         <Component {...pageProps} />
       </main>
+      <ToastContainer />
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
 
