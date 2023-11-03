2100bd41c2a28107103ef4cdbba4494d105a2497
Sixtus Odom <sixtusodom2020@gmail.com>
2023-11-03 19:04:13 +0100

    fix: user profile

--- src/components/organisms/UserProfile/CardInfo.tsx
+++ src/components/organisms/UserProfile/CardInfo.tsx
@@ -1,25 +1,25 @@
-// import React from "react";
+import React from "react";
 
-// const CardInfo = () => {
-//   return (
-//     <div>
-//       <div className="max-w-[1280px] xl:ml-40">
-//         <div className="mt-4 h-48 rounded-xl bg-[#E8E8E8]">
-//           <div className="space-y-7 px-10 py-8">
-//             <p className="text-base font-normal">
-//               Billed to: <span>5676*******754</span>
-//             </p>
-//             <p className="text-base font-normal">
-//               Suitemate subscription: <span>USD $25/month</span>
-//             </p>
-//             <p className="text-base font-normal">
-//               Next payment: <span className="text-[#3BAF75]">April 10, 2023</span>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const CardInfo = () => {
+  return (
+    <div>
+      <div className="grid max-w-[1180px] md:grid-cols-2 lg:mx-auto">
+        <div className="mt-4 h-48 rounded-xl bg-[#E8E8E8]">
+          <div className="space-y-7 px-10 py-8">
+            <p className="text-base font-normal">
+              Billed to: <span>5676*******754</span>
+            </p>
+            <p className="text-base font-normal">
+              Suitemate subscription: <span>USD $25/month</span>
+            </p>
+            <p className="text-base font-normal">
+              Next payment: <span className="text-[#3BAF75]">April 10, 2023</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default CardInfo;
+export default CardInfo;
